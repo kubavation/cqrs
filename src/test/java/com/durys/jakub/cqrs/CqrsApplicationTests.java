dcae313f7adeb0ebb3787ab25350fede8ba9a46c
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-07 09:21:11 +0200

    init

---
+++ src/test/java/com/durys/jakub/cqrs/CqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.cqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
